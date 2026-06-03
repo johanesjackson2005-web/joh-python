90fb3c036d59e81bae7a766e7f61c09a6ddc3102
Johanes Jackson <johanesjackson2005@gmail.com>
2026-06-03 12:30:29 +0300

    Update admin.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.contrib.auth.models import User
 from django.contrib.auth.admin import UserAdmin
+from .models import Contact
+
+admin.site.register(Contact)
 
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
