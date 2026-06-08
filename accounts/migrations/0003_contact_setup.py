07da7c34c616222273e007f4bb78966785ebc310
Johanes Jackson <johanesjackson2005@gmail.com>
2026-06-08 19:31:22 +0300

    change

--- accounts/migrations/0003_contact_setup.py
+++ accounts/migrations/0003_contact_setup.py
@@ -10,23 +10,4 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.CreateModel(
-            name='Contact',
-            fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('username', models.CharField(max_length=100)),
-                ('email', models.EmailField(max_length=254)),
-                ('message', models.TextField()),
-                ('created_at', models.DateTimeField(auto_now_add=True)),
-                ('admin_reply', models.TextField(blank=True, null=True)),
-            ],
-        ),
-        migrations.CreateModel(
-            name='Setup',
-            fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(max_length=100)),
-                ('download_url', models.URLField()),
-            ],
-        ),
     ]
