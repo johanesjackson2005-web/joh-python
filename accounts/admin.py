914b522afb5bbdb1cddb0fc76b81e8a271800a25
Johanes Jackson <johanesjackson2005@gmail.com>
2026-06-08 18:31:03 +0300

    change

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 from django.contrib.auth.models import User
 from django.contrib.auth.admin import UserAdmin
 from .models import Contact
-
+admin.site.site_header = "JOHBOY SETUPS ADMIN"
+admin.site.site_title = "JOHBOY SETUPS"
+admin.site.index_title = "Welcome to JOHBOY SETUPS Dashboard"
 admin.site.register(Contact)
 
 admin.site.unregister(User)
