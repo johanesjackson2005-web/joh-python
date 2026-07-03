f53012cfe4c3f32f185558255d3c997af5f6ca48
Johanes Jackson <johanesjackson2005@gmail.com>
2026-07-03 16:15:03 +0300

    change

--- env/Scripts/activate.fish
+++ env/Scripts/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV 'C:\Users\ANESIA\Desktop\joh python\env'
+set -gx VIRTUAL_ENV 'C:\Users\JOHBOY\OneDrive\Desktop\joh-python\env'
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"Scripts $PATH
