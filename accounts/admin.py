1bb70d4696ca85abce5b0163efb44ff5a9e65169
Johanes Jackson <johanesjackson2005@gmail.com>
2026-07-04 01:31:53 +0300

    change

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.models import User
 from django.contrib.auth.admin import UserAdmin
 from .models import Contact
+from .models import Category, Software
 admin.site.site_header = "JOHBOY SETUPS ADMIN"
 admin.site.site_title = "JOHBOY SETUPS"
 admin.site.index_title = "Welcome to JOHBOY SETUPS Dashboard"   
@@ -21,3 +22,8 @@ class ContactAdmin(admin.ModelAdmin):
         'message',
         'admin_reply'
     )
+
+
+
+admin.site.register(Category)
+admin.site.register(Software)
--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
 class Background(models.Model):
     title = models.CharField(max_length=100)
     image = models.ImageField(upload_to='backgrounds/')
@@ -21,3 +22,27 @@ class Contact(models.Model):
     message = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     admin_reply = models.TextField(blank=True, null=True)
+    
+   
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to="categories/", blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Software(models.Model):
+    
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    version = models.CharField(max_length=30, blank=True)
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to="software/", blank=True, null=True)
+    download_link = models.URLField()
+    size = models.CharField(max_length=30, blank=True)
+
+    def __str__(self):
+        return self.name
