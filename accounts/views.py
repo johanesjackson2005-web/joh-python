29a9160aebc7d8d2a3e9cd5df3e1828f188c8804
Johanes Jackson <johanesjackson2005@gmail.com>
2026-07-04 13:45:01 +0300

    CHANGE

--- accounts/views.py
+++ accounts/views.py
@@ -270,41 +270,36 @@ def livestreams(request):
             "livestreams": livestreams
         }
     )
-
-
 def search_view(request):
-    query = request.GET.get("q", "").strip()
+    query = request.GET.get("q", "")
 
-    # split query into tokens (important upgrade)
-    tokens = query.split()
+    tutorial_results = Tutorial.objects.filter(
+        Q(title__icontains=query) |
+        Q(description__icontains=query) |
+        Q(instructor__icontains=query) |
+        Q(category__name__icontains=query)
+    )
 
-    software_q = Q()
-    tutorial_q = Q()
-    category_q = Q()
-    live_q = Q()
+    software_results = Software.objects.filter(
+        Q(name__icontains=query) |
+        Q(description__icontains=query) |
+        Q(category__name__icontains=query)
+    )
 
-    for word in tokens:
-        software_q |= Q(name__icontains=word)
+    category_results = Category.objects.filter(
+        Q(name__icontains=query) |
+        Q(description__icontains=query)
+    )
 
-        tutorial_q = Q(software__name__icontains=query) | Q(title__icontains=query)
+    livestream_results = LiveStream.objects.filter(
+        Q(title__icontains=query) |
+        Q(description__icontains=query)
+    )
 
-        category_q |= Q(name__icontains=word)
-
-        live_q |= (
-            Q(title__icontains=word) |
-            Q(software__name__icontains=word) |
-            Q(category__name__icontains=word)
-        )
-
-    software_results = Software.objects.filter(software_q).distinct()
-    tutorial_results = Tutorial.objects.filter(tutorial_q).distinct()
-    category_results = Category.objects.filter(category_q).distinct()
-    live_results = LiveStream.objects.filter(live_q).distinct()
-
-    return render(request, "search_results.html", {
+    return render(request, "search.html", {
         "query": query,
-        "software_results": software_results,
         "tutorial_results": tutorial_results,
+        "software_results": software_results,
         "category_results": category_results,
-        "live_results": live_results
+        "livestream_results": livestream_results,
     })
